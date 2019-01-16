47e0a485bf2b760e9a2ce8608c82c41ff74f440c
Hannah Herbst <hherbst86@gmail.com>
2019-01-16 15:08:20 -0500

    Update chapter-exercises.js

--- eloquent-javascript/chapter-exercises.js
+++ eloquent-javascript/chapter-exercises.js
@@ -1,9 +1,22 @@
-/* Write a function arrayToList that builds up a list structure like the one shown when given [1, 2, 3] as argument. Also write a listToArray function that produces an array from a list. Then add a helper function prepend, which takes an element and a list and creates a new list that adds the element to the front of the input list, and nth, which takes a list and a number and returns the element at the given position in the list (with zero referring to the first element) or undefined when there is no such element.
+/* 
 
-If you haven’t already, also write a recursive version of nth. */
+ELOQUENT JAVASCRIPT CHAPTER 4: Solutions
+
+Please note, I did not save the solutions to previous problems but wanted to for these because they were most helpful in learning. 
+
+The following list is enumerated for reference to the solutions:
+
+1. Write a function arrayToList that builds up a list structure like the one shown when given [1, 2, 3] as argument. 
+
+2. Also write a listToArray function that produces an array from a list. 
+
+3. Then add a helper function prepend, which takes an element and a list and creates a new list that adds the element to the front of the input list, and nth, which takes a list and a number and returns the element at the given position in the list (with zero referring to the first element) or undefined when there is no such element.
+
+4. If you haven’t already, also write a recursive version of nth. */
 
 //https://jsbin.com/lalegizeya/edit?js,console,output
 
+//SOLUTION 1
 
 function arrayToList(arr){  
   let obj = null; 
@@ -17,6 +30,7 @@ function arrayToList(arr){
 return (obj); 
 } 
 
+//SOLUTION 2
 
 function listToArray(obj){ 
   let objSearched = obj.rest;
@@ -30,6 +44,7 @@ function listToArray(obj){
   return (arr);
 }
 
+//SOLUTION 3
 
 function prepend(listOrArray,input){
  
@@ -48,7 +63,39 @@ function prepend(listOrArray,input){
   
   else {
     console.log("Please submit an array or an object.")
-  }
-  
+  }  
   
 }
+
+//SOLUTION 4
+
+function nth(listOrArray, input){
+  
+  let i = 0;
+  let x;
+  
+  if(Array.isArray(listOrArray)){
+    while (i < listOrArray.length){
+      if (input == listOrArray[i]){
+        x = i;
+        return console.log(input + " is at position " + x);
+      }
+      i++;
+    }
+    return console.log(x);
+  }
+  
+  else if(listOrArray.toString() == "[object Object]"){
+    listOrArray = listToArray(listOrArray);
+    return nth(listOrArray, input);
+  }
+  
+  else {
+    console.log("Please submit an array or an object.")
+  }
+  
+  return console.log(x + " is x")
+  
+}
+
+//Note to self: I should probably make a function that validates function parameters before trying to run them and lets the user know they aren't gonna work!
