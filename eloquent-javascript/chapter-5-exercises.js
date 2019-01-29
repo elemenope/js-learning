4b45b4c71660000f52d351a5ee523cd03d973780
Hannah Herbst <hherbst86@gmail.com>
2019-01-29 12:34:21 -0500

    Update chapter-5-exercises.js

--- eloquent-javascript/chapter-5-exercises.js
+++ eloquent-javascript/chapter-5-exercises.js
@@ -57,3 +57,25 @@ let loop = (inputArray, startingValue, endingValue) => {
   newArr = inputArray.map(index => endingValue);
   return newArr;
 }
+
+
+//Analogous to the some method, arrays also have an every method. This one returns true when the given function 
+//returns true for every element in the array. In a way, some is a version of the || operator that acts on 
+//arrays, and every is like the && operator.
+
+//Implement every as a function that takes an array and a predicate function as parameters. 
+//Write two versions, one using a loop and one using the some method.
+
+//DEMO: https://jsbin.com/feqebinigu/edit?js,console
+
+let every = (array, testFunction) => {
+  return arr.some(testFunction);
+}
+
+
+let everyLoop = (array, testFunction) => {
+  let value = 0;
+  for (let value in array){
+    return testFunction(array[value]);
+  }
+}
