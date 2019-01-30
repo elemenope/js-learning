5f26ee00cdb652959682fdeec56fd1128cf4ded9
Hannah Herbst <hherbst86@gmail.com>
2019-01-30 14:25:59 -0500

    Update chapter-5-exercises.js

--- eloquent-javascript/chapter-5-exercises.js
+++ eloquent-javascript/chapter-5-exercises.js
@@ -79,3 +79,55 @@ let everyLoop = (array, testFunction) => {
     return testFunction(array[value]);
   }
 }
+
+//Write a function that computes the dominant writing direction in a string of text. Remember that 
+//each script object has a direction property that can be "ltr" (left to right), "rtl" (right to left), 
+//or "ttb" (top to bottom).
+
+//The dominant direction is the direction of a majority of the characters that have a script associated with 
+//them. The characterScript and countBy functions defined earlier in the chapter are probably useful here.
+
+//DEMO: https://jsbin.com/geqopovupe/edit?js,console
+
+
+function dominantDirection(text) {
+  
+  let unicodeCollection = text.split('').map(function(num){
+    return num.charCodeAt(0);
+  },0);
+  
+  let directionComparison = [];
+
+  unicodeCollection.forEach((item) => {
+    for (let key in SCRIPTS){
+      for (let index in SCRIPTS[key].ranges){
+        if (item >= (SCRIPTS[key].ranges[index][0]) && item <= (SCRIPTS[key].ranges[index][1])){
+          directionComparison.push(SCRIPTS[key].direction);
+        }
+      }
+    }
+    return directionComparison;
+  });
+
+  let valueOfDirection = directionComparison.reduce((accumulator, currentValue) => { 
+        currentValue in accumulator ? 
+            accumulator[currentValue]++ : accumulator[currentValue] = 1;
+        return accumulator;
+    }, {});
+  
+  
+  valueOfDirection = (Object.entries(valueOfDirection));
+  
+  while(valueOfDirection.length > 1){
+    (valueOfDirection[0][1] > valueOfDirection[1][1]) ? valueOfDirection.splice(1, 1) : valueOfDirection.splice(0, 1);
+  }
+  
+  return valueOfDirection[0][0];
+
+}
+
+console.log(dominantDirection("Hello!"));
+// → ltr
+
+console.log(dominantDirection("Hey, مساء الخير"));
+// → rtl
